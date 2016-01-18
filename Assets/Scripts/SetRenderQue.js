a440499f485c9550e8d4230f22491dc41bb7f74b
TheodoreNguyen <theodore.h.nguyen@outlook.com>
2016-01-17 20:38:05 -0800

    add all updated files with UI

---
+++ Assets/Scripts/SetRenderQue.js
@@ -0,0 +1,19 @@
+﻿var queue : int = 2000; //3000 is default in Unity
+var applyToChildren : boolean = false;
+var renderer1;
+function Awake(){
+	renderer1 = GetComponent(Renderer);
+	SetRenderQueue();
+}
+ 
+function SetRenderQueue(){
+	if (!renderer1 || !renderer1.sharedMaterial || applyToChildren){
+		if(applyToChildren){
+			for(var child : Transform in transform){
+				child.renderer1.sharedMaterial.renderQueue = queue;
+			}
+		} else { 
+			print("No renderer found on this GameObject. Check the applyToChildren box to apply settings to children"); 
+		} 	 
+	} else {renderer1.sharedMaterial.renderQueue = queue;}
+}
---
+++ Assets/Scripts/StartView.js
@@ -0,0 +1,55 @@
+﻿var ray: Ray;
+var hit: RaycastHit;
+
+var m1: GameObject;
+var m2: GameObject;
+var m3: GameObject;
+var m4: GameObject;
+var m5: GameObject;
+var m6: GameObject;
+var m7: GameObject;
+
+function Start() {
+	
+	m1 = GameObject.FindGameObjectWithTag("Instructions");
+	m2 = GameObject.FindGameObjectWithTag("Easy");
+	m3 = GameObject.FindGameObjectWithTag("Medium");
+	m4 = GameObject.FindGameObjectWithTag("Hard");
+	m5 = GameObject.FindGameObjectWithTag("MenToCred");
+	m6 = GameObject.FindGameObjectWithTag("Credits");
+	m7 = GameObject.FindGameObjectWithTag("CredToMenu");
+	m6.SetActive(false);
+	m7.SetActive(false);
+	
+}
+
+
+function Update() {
+	if (Input.GetKey(KeyCode.C)){
+		m1.SetActive(false);
+		m2.SetActive(false);
+		m3.SetActive(false);
+		m4.SetActive(false);
+		m5.SetActive(false);
+		m6.SetActive(true);
+		m7.SetActive(true);
+	}
+	if (Input.GetKey(KeyCode.M)){
+		m1.SetActive(true);
+		m2.SetActive(true);
+		m3.SetActive(true);
+		m4.SetActive(true);
+		m5.SetActive(true);
+		m6.SetActive(false);
+		m7.SetActive(false);
+	}
+    if (Input.GetKey(KeyCode.Mouse0)) {
+        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if(Physics.Raycast(ray,hit)){
+            if(hit.rigidbody.name=="Easy") Application.LoadLevel ("Easy");
+            if(hit.rigidbody.name=="Medium") Application.LoadLevel ("Medium");
+            if(hit.rigidbody.name=="Hard") Application.LoadLevel ("Hard");
+		//	if(hit.rigidbody.name=="Credits") Application.LoadLevel("Credits");
+        }
+    }
+}
